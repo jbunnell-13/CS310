13990d4bd2f07134480677aa3a378b5c736c9bc2
jbunnell-13 <jbunnell13@gmail.com>
2026-04-17 02:39:09 -0600

    Week 5 errors

---
+++ Week5Errors.cpp
@@ -0,0 +1,30 @@
+/*
+* Student Name: Jesse Bunnell
+* File Name: Week5Errors.cpp
+* Date: 04/15/2026
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+// Function to add two numbers
+int addNumbers(int x, int y)
+{
+    int result = x + y
+    return result;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    // Call the function
+    int sum = addNumber(a, b);
+
+    cout << "Sum is: " << sum << endl
+
+    return 0;
+}
